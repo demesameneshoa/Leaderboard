1296a001954719ab0d37ffea3acc7579d63520e5
demesameneshoa <demesameneshoa@gmail.com>
2023-06-20 23:20:45 +0300

    add linters to project

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   entry: './src/index.js',
   devServer: {
@@ -13,7 +14,7 @@ module.exports = {
   output: {
     filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
-    chunkFilename: '[id].[chunkhash].js'
+    chunkFilename: '[id].[chunkhash].js',
   },
   module: {
     rules: [
@@ -26,4 +27,5 @@ module.exports = {
   optimization: {
     runtimeChunk: 'single',
   },
+  mode: 'development',
 };
